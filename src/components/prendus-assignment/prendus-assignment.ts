b3e842d3c1c436b6804e4b0580c0c623cd2edd89
Jordan Last <jordan.michael.last@gmail.com>
2017-06-21 11:00:27 -0600

    conditionally load different assignment components

--- src/components/prendus-assignment/prendus-assignment.ts
+++ src/components/prendus-assignment/prendus-assignment.ts
@@ -7,6 +7,7 @@ import {Concept} from '../../typings/concept';
 import {User} from '../../typings/user';
 import {checkForUserToken, getAndSetUser} from '../../redux/actions';
 import {createUUID} from '../../services/utilities-service';
+import {AssignmentType} from '../../typings/assignment-type';
 
 class PrendusAssignment extends Polymer.Element implements ContainerElement {
     componentId: string;
@@ -20,6 +21,8 @@ class PrendusAssignment extends Polymer.Element implements ContainerElement {
     learningStructure: any;
     subjects: Subject[];
     concepts: Concept[];
+    assignmentType: AssignmentType;
+    connected: boolean;
 
     static get is() { return 'prendus-assignment'; }
     static get properties() {
@@ -32,6 +35,9 @@ class PrendusAssignment extends Polymer.Element implements ContainerElement {
             },
             mode: {
 
+            },
+            assignmentType: {
+                observer: 'assignmentTypeChanged'
             }
         };
     }
@@ -43,6 +49,14 @@ class PrendusAssignment extends Polymer.Element implements ContainerElement {
 
     async connectedCallback() {
         super.connectedCallback();
+
+        this.action = {
+            type: 'SET_COMPONENT_PROPERTY',
+            componentId: this.componentId,
+            key: 'connected',
+            value: true
+        };
+
         this.action = {
             type: 'SET_COMPONENT_PROPERTY',
             componentId: this.componentId,
@@ -70,6 +84,24 @@ class PrendusAssignment extends Polymer.Element implements ContainerElement {
         await this.loadData();
         await this.loadLearningStructure();
     }
+
+    assignmentTypeChanged() {
+        this.action = {
+            type: 'SET_COMPONENT_PROPERTY',
+            componentId: this.componentId,
+            key: 'assignmentType',
+            value: this.assignmentType
+        };
+    }
+
+    isCreateType(assignmentType) {
+        return assignmentType === 'CREATE';
+    }
+
+    isReviewType(assignmentType) {
+        return assignmentType === 'REVIEW';
+    }
+
     showSubjects(e){
       //Setting this here because we don't want to show concepts that aren't aligned with a Subject. I assume this is the best way to do it?
       if(this.concepts){
@@ -186,11 +218,13 @@ class PrendusAssignment extends Polymer.Element implements ContainerElement {
     stateChange(e: CustomEvent) {
         const state = e.detail.state;
         if (Object.keys(state.components[this.componentId] || {}).includes('loaded')) this.loaded = state.components[this.componentId].loaded;
+        if (Object.keys(state.components[this.componentId] || {}).includes('connected')) this.connected = state.components[this.componentId].connected;
         if (Object.keys(state.components[this.componentId] || {}).includes('assignmentId')) this.assignmentId = state.components[this.componentId].assignmentId;
         if (Object.keys(state.components[this.componentId] || {}).includes('subjects')) this.subjects = state.components[this.componentId].subjects;
         if (Object.keys(state.components[this.componentId] || {}).includes('concepts')) this.concepts = state.components[this.componentId].concepts;
         if (Object.keys(state.components[this.componentId] || {}).includes('learningStructure')) this.learningStructure = state.components[this.componentId].learningStructure;
         if (Object.keys(state.components[this.componentId] || {}).includes('learningStructure')) this.learningStructure = state.components[this.componentId].learningStructure;
+        if (Object.keys(state.components[this.componentId] || {}).includes('assignmentType')) this.assignmentType = state.components[this.componentId].assignmentType;
         this.assignment = state[`assignment${this.assignmentId}`];
         this.userToken = state.userToken;
         this.user = state.user;
---
+++ src/typings/assignment-type.ts
@@ -0,0 +1,1 @@
+export type AssignmentType = 'CREATE' | 'REVIEW';
