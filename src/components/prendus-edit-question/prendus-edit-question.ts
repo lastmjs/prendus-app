f693fac1508707393b4a4b69182443757aad2b75
Jordan Last <jordan.michael.last@gmail.com>
2017-07-07 19:24:52 -0600

    responsiveness

--- src/components/prendus-edit-question/prendus-edit-question.ts
+++ src/components/prendus-edit-question/prendus-edit-question.ts
@@ -51,6 +51,10 @@ class PrendusEditQuestion extends Polymer.Element {
             key: 'loaded',
             value: true
         };
+
+        setTimeout(() => { //TODO fix this...it would be nice to be able to set the font-size officially through the ace editor web component, and then we wouldn't have to hack. The timeout is to ensure the current task on the event loop completes and the dom template is stamped because of the loaded property before accessing the dom
+            this.shadowRoot.querySelector('#codeEditor').shadowRoot.querySelector('#juicy-ace-editor-container').style = 'font-size: calc(40px - 1vw)';
+        }, 1000);
     }
 
     textEditorChanged() {
