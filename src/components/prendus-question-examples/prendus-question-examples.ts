a0a4524b560f657d56e08eadf148f35f48e3c7bb
Jordan Last <jordan.michael.last@gmail.com>
2017-07-11 13:38:58 -0600

    adding examples page for more complex examples of what the question can do

---
+++ src/components/prendus-question-examples/prendus-question-examples.ts
@@ -0,0 +1,5 @@
+class PrendusQuestionExamples extends Polymer.Element {
+    static get is() { return 'prendus-question-examples'; }
+}
+
+window.customElements.define(PrendusQuestionExamples.is, PrendusQuestionExamples);
