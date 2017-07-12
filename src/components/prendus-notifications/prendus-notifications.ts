aca7473202277f4ffd57b99dc6aa6df0be2988b6
Trevor <trevoranthonylane@gmail.com>
2017-07-12 10:52:33 -0600

    working on fixing landing page media query

---
+++ src/components/prendus-notifications/prendus-notifications.ts
@@ -0,0 +1,5 @@
+class PrendusNotifications extends Polymer.Element {
+    static get is() { return 'PrendusNotifications'; }
+}
+
+window.customElements.define(PrendusNotifications.is, PrendusNotifications);
