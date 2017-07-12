1313eb5e66f853c411a16c2668bf1e0efc6d53fb
Trevor <trevoranthonylane@gmail.com>
2017-07-12 12:54:02 -0600

    removed prendus notification files, just trying to upload changed to landing page

--- src/components/prendus-notifications/prendus-notifications.ts
+++
@@ -1,5 +0,0 @@
-class PrendusNotifications extends Polymer.Element {
-    static get is() { return 'PrendusNotifications'; }
-}
-
-window.customElements.define(PrendusNotifications.is, PrendusNotifications);
