b9757556b4c5752ada54f3f8a761568ec10546b1
Ben Mackley <benjaminmackley@gmail.com>
2017-07-28 16:20:29 -0600

    working in getting the concept down right

--- src/components/prendus-course/prendus-course.ts
+++ src/components/prendus-course/prendus-course.ts
@@ -1,6 +1,8 @@
 import {GQLQuery, GQLMutate, GQLSubscribe} from '../../services/graphql-service';
 import {ContainerElement} from '../../typings/container-element';
 import {Mode} from '../../typings/mode';
+import {Subject} from '../../typings/subject';
+import {Discipline} from '../../typings/discipline';
 import {SetPropertyAction, SetComponentPropertyAction, DefaultAction} from '../../typings/actions';
 import {Assignment} from '../../typings/assignment';
 import {Course} from '../../typings/course';
@@ -14,11 +16,14 @@ class PrendusCourse extends Polymer.Element implements ContainerElement {
     componentId: string;
     action: SetPropertyAction | SetComponentPropertyAction | DefaultAction;
     assignments: Assignment[];
+    learningStructure: any;
     course: Course;
     loaded: boolean;
     userToken: string;
     user: User;
     editingTitle: boolean;
+    subjects: Subject[];
+    selectedDiscipline: Discipline;
 
     static get is() { return 'prendus-course'; }
     static get properties() {
@@ -67,7 +72,86 @@ class PrendusCourse extends Polymer.Element implements ContainerElement {
     isCreateMode(mode: Mode) {
         return mode === 'create';
     }
-
+    openCreateDisciplineDialog(e){
+      console.log('openingn')
+      this.shadowRoot.querySelector('#create-discipline').open();
+    }
+    async createDiscipline(){
+      const data = await GQLMutate(`
+          mutation {
+              createDiscipline(
+                  title: "${this.shadowRoot.querySelector('#discipline-title').value}"
+              ) {
+                  id
+                  title
+              }
+          }
+      `, this.userToken, (error: any) => {
+        console.log('error', error)
+      });
+      //TODO combine this with the creatediscipline above
+      this.saveDiscipline(data.createDiscipline.id);
+      if(this.subjects){
+        this.action = {
+            type: 'SET_COMPONENT_PROPERTY',
+            componentId: this.componentId,
+            key: 'subjects',
+            value: null
+        };
+      }
+    }
+    //TODO Allow the user to only create one discipline, but allow them to edit that discipline if necessary
+    async saveDiscipline(disciplineId: string){
+      const courseData = await GQLMutate(`
+        mutation {
+          addToCourseDiscipline(
+            coursesCourseId: "${this.courseId}"
+            disciplineDisciplineId: "${disciplineId}"
+          ) {
+        		disciplineDiscipline{
+              title
+              subjects{
+                id
+                title
+              }
+            }
+          }
+        }
+      `, this.userToken, (error: any) => {
+        console.log('error', error)
+      });
+      this.loadLearningStructure();
+      this.shadowRoot.querySelector('#subject-menu').disabled = false;
+      this.shadowRoot.querySelector('#create-discipline').close();
+    }
+    showSubjects(e){
+      //Setting this here because we don't want to show concepts that aren't aligned with a Subject. I assume this is the best way to do it?
+      this.shadowRoot.querySelector('#subject-menu').disabled = false;
+      this.saveDiscipline(this.learningStructure[e.target.id].id);
+      this.action = {
+          type: 'SET_COMPONENT_PROPERTY',
+          componentId: this.componentId,
+          key: 'subjects',
+          value: this.learningStructure[e.target.id].subjects
+      };
+    }
+    openCreateSubjectDialog(e){
+      this.shadowRoot.querySelector('#create-subject').open();
+    }
+    async saveSubject(){
+      const data = await GQLMutate(`
+          mutation {
+              createSubject(
+                  title: "${this.shadowRoot.querySelector('#subject-title').value}"
+              ) {
+                  id
+              }
+          }
+      `, this.userToken, (error: any) => {
+        console.log('error', error)
+          alert(error);
+      });
+    }
     async courseIdChanged() {
         this.action = {
             type: 'SET_COMPONENT_PROPERTY',
@@ -84,7 +168,7 @@ class PrendusCourse extends Polymer.Element implements ContainerElement {
         };
 
         await this.loadData();
-
+        await this.loadLearningStructure();
         this.action = {
             type: 'SET_COMPONENT_PROPERTY',
             componentId: this.componentId,
@@ -188,21 +272,47 @@ class PrendusCourse extends Polymer.Element implements ContainerElement {
                     }
                 ) {
                     id
+                    title
                 }
             }
         `, this.userToken, (error: any) => {
             console.log(error);
         });
-
+        navigate(`/course/${data.updateOrCreateCourse.id}/edit`)
+    }
+    async loadLearningStructure(){
+      await GQLQuery(`
+        query {
+          allDisciplines(
+            first: 25
+            filter: {
+              OR: [{
+                  courses_some: {
+                  id: "${this.courseId}"
+                }
+              },{
+                approved_in:YES
+              }]
+          }) {
+            id
+            title
+            subjects{
+              title
+            }
+          }
+        }
+      `, this.userToken, (key: string, value: any) => {
+        console.log('value', value)
         this.action = {
             type: 'SET_COMPONENT_PROPERTY',
             componentId: this.componentId,
-            key: 'courseId',
-            value: data.updateOrCreateCourse.id
+            key: 'learningStructure',
+            value
         };
-        navigate(`/course/${data.updateOrCreateCourse.id}/edit`)
+      }, (error: any) => {
+          console.log(error);
+      });
     }
-
     stateChange(e: CustomEvent) {
         const state = e.detail.state;
 
@@ -210,7 +320,10 @@ class PrendusCourse extends Polymer.Element implements ContainerElement {
         if (Object.keys(state.components[this.componentId] || {}).includes('loaded')) this.loaded = state.components[this.componentId].loaded;
         if (Object.keys(state.components[this.componentId] || {}).includes('assignments')) this.assignments = state.components[this.componentId].assignments;
         if (Object.keys(state.components[this.componentId] || {}).includes('course')) this.course = state.components[this.componentId].course;
-        // this.assignments = state[`assignmentsFromCourse${this.courseId}`];
+        if (Object.keys(state.components[this.componentId] || {}).includes('learningStructure')) this.learningStructure = state.components[this.componentId].learningStructure;
+        if (Object.keys(state.components[this.componentId] || {}).includes('subjects')) this.subjects = state.components[this.componentId].subjects;
+        if (Object.keys(state.components[this.componentId] || {}).includes('selectedDiscipline')) this.selectedDiscipline = state.components[this.componentId].selectedDiscipline;
+//      this.assignments = state[`assignmentsFromCourse${this.courseId}`];
         // this.course = state[`course${this.courseId}`];
         this.userToken = state.userToken;
         this.user = state.user;
