e66e61a0c4fe2f15f29e0dec43d9102c60cf4e66
Benjamin Ogles <benjaminogles@gmail.com>
2017-08-14 12:52:22 -0600

    First draft of the editable rubric table

--- src/components/prendus-rubric-table/prendus-rubric-table.ts
+++ src/components/prendus-rubric-table/prendus-rubric-table.ts
@@ -10,6 +10,12 @@ class PrendusRubricTable extends Polymer.Element {
   user: User;
 
   static get is() { return 'prendus-rubric-table' }
+  static get properties() {
+    return {
+      editable: Boolean,
+      rubric: String //JSON string
+    }
+  }
 
   constructor() {
     super();
@@ -25,9 +31,45 @@ class PrendusRubricTable extends Polymer.Element {
     };
   }
 
+  templateRubric(): Object[] {
+    return [
+      {
+        name: '',
+        scales: this.templateScale()
+      }
+    ];
+  }
+
+  templateScale(): Object[] {
+    return [
+      {
+        name: '',
+        description: '',
+        points: 0
+      }
+    ]
+  }
+
   connectedCallback() {
     super.connectedCallback();
+    this.rubric || this._fireLocalAction('rubric', this.editable ? this.templateRubric() : '');
     this._fireLocalAction('loaded', true);
+  }
+
+  addCategory() {
+    this._fireLocalAction('rubric', this.rubric.concat(this.templateRubric()));
+  }
+
+  addScale(e) {
+    const newRubric = this.rubric.map((category, i) => {
+      if (i === e.model.itemsIndex)
+        return {
+          name: category.name,
+          scales: category.scales.concat(this.templateScale())
+        }
+      return category;
+    });
+    this._fireLocalAction('rubric', newRubric);
   }
 
   stateChange(e: CustomEvent) {
@@ -35,6 +77,7 @@ class PrendusRubricTable extends Polymer.Element {
     const componentState = state.components[this.componentId] || {};
     const keys = Object.keys(componentState);
     if (keys.includes('loaded')) this.loaded = componentState.loaded;
+    if (keys.includes('rubric')) this.rubric = componentState.rubric;
     this.userToken = state.userToken;
     this.user = state.user;
   }
