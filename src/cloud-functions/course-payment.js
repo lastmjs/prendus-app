804249dea0adceeb91e7f79f2a7f711e393e6165
Jordan Last <jordan.michael.last@gmail.com>
2017-08-26 15:06:28 -0600

    add cloud function

---
+++ src/cloud-functions/course-payment.js
@@ -0,0 +1,4 @@
+//TODO validate that the user is on the course
+//TODO get the course price
+//TODO use the tokenId to create the payment
+//TODO If you are unsure if a Transaction is erroneous or suspicious, you agree to research the Transaction and, if necessary, contact your Customer before fulfilling or completing the Transaction.
