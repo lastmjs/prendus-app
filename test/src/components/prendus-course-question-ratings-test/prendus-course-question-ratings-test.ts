8555a7aebab35865f361d4d6f0c654585b16932c
Benjamin Ogles <benjaminogles@gmail.com>
2017-09-01 10:51:53 -0600

    Test services

--- test/src/components/prendus-course-question-ratings-test/prendus-course-question-ratings-test.ts
+++ test/src/components/prendus-course-question-ratings-test/prendus-course-question-ratings-test.ts
@@ -1,76 +1,113 @@
-import {GQLRequest} from '../../../../src/node_modules/prendus-shared/services/graphql-service';
 import {createUUID} from '../../../../src/node_modules/prendus-shared/services/utilities-service';
-import {GQLVariables} from '../../../../src/typings/gql-variables';
 import {RootReducer} from '../../../../src/redux/reducers';
 import {PrendusCourseQuestionRatings} from '../../../../src/components/prendus-course-question-ratings/prendus-course-question-ratings';
 import {DEFAULT_EVALUATION_RUBRIC} from '../../../../src/services/constants-service';
+import {CourseArb} from '../../services/arbitraries-service';
+import {saveArbitrary, authenticateTestUser, deleteTestUsers} from '../../services/dataGen-service';
 
-const jsc = require('jsverify');
-const deepEqual = require('deep-equal');
+class PrendusCourseQuestionRatingsTest extends Polymer.Element {
 
-//Test users and constants
-const JWT = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MDQxOTcxNzMsImNsaWVudElkIjoiY2o1bDg3cmQwMzVoaTAxMzQ0bzAwNW5maCIsInByb2plY3RJZCI6ImNqNW12aXNoaW5ucGYwMTM0OG04Z3p0YjAiLCJwZXJtYW5lbnRBdXRoVG9rZW5JZCI6ImNqNzBvNXJldTA0ZmEwMTk4a2ZlNnkwaXIifQ.LbuydRKQjQgbMiMggFU-wOr-IKSxzcO5ZA5mAZGEUjU';
-const studentId = 'cj70ofsne1dbq0100o06o4zoy';
-const instructorId = 'cj70oeaw01b880100p9zjxsyo';
-const TYPE_MAP = {
-  scores: 'CategoryScore',
-  concept: 'Concept',
-  assignments: 'Assignment',
-  questions: 'Question',
-  ratings: 'QuestionRating',
-  discipline: 'Discipline',
-  subject: 'Subject'
-};
+  static get is() { return 'prendus-course-question-ratings-test' }
 
-//Utility functions
-function handleError(err: any) {
-  console.error(err);
+  constructor() {
+    super();
+    this.componentId = createUUID();
+    this.rootReducer = RootReducer;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.action = {
+      type: 'SET_COMPONENT_PROPERTY',
+      componentId: this.componentId,
+      key: 'table',
+      value: this.shadowRoot.querySelector('#table')
+    };
+  }
+
+  prepareTests(test) {
+
+    test('Set course id without residual state', [CourseArb], async (course: Course) => {
+      const student = await authenticateTestUser('STUDENT');
+      const instructor = await authenticateTestUser('INSTRUCTOR');
+      const ids = await saveArbitrary(course);
+      await deleteTestUsers(student, instructor);
+      return true;
+      //      const courseData = await saveCourse(course);
+      //      const table = new PrendusCourseQuestionRatings();
+      //      this.shadowRoot.appendChild(table);
+      //      const fulfilled = setUpListener(table);
+      //      table.courseId = courseData.id;
+      //      console.log('awaiting', time());
+      //      await fulfilled;
+      //      console.log('waited', time());
+      //      const success = verifyTable(table, courseData);
+      //      this.shadowRoot.removeChild(table);
+      //      await deleteCourse(courseData);
+      //      return success;
+    });
+
+    //    test('Set course id with residual state', [coursesArb], async (courses: Courses) => {
+    //      const coursesData = await saveCourses(courses);
+    //      console.log(coursesData);
+    //      let success = true;
+    //      coursesData.forEach(async (course) => {
+    //        const fulfilled = setUpListener(this.table);
+    //        this.table.courseId = courseId;
+    //        await fulfilled;
+    //        success = success && verifyTable(this.table, course);
+    //      });
+    //      await Promise.all(coursesData.map(deleteCourse));
+    //      return success;
+    //    });
+
+    //    test('Set assignment id and concept id', [courseArb], async (course: Course) => {
+    //      const courseData = await saveCourse(course);
+    //      const fulfilled = setUpListener(this.table);
+    //      this.table.courseId = courseData.id;
+    //      await fulfilled;
+    //      let success = true;
+    //      const conceptIds = ['All', ...this.table._concepts(courseData.assignments).map(concept => concept.id)];
+    //      const assignmentIds = ['All', ...courseData.assignments.map(assignment => assignment.id)];
+    //      assignmentIds.forEach(assignmentId => {
+    //        conceptIds.forEach(conceptId => {
+    //          const filter = this.table._makeFilter(assignmentId, conceptId);
+    //          const filtered = this.table.questionStats.filter(filter);
+    //          success = success && verifyFilter(assignmentId, conceptId, filtered);
+    //        });
+    //      });
+    //      await deleteCourse(courseData);
+    //      return success;
+    //    });
+    //
+    //    test('Sort columns', [courseArb], async (course: Course) => {
+    //      const courseData = await saveCourse(course);
+    //      const fulfilled = setUpListener(this.table);
+    //      this.table.courseId = courseData.id;
+    //      await fulfilled;
+    //      let success = true;
+    //      const columns = ['Overall', 'Student', ...Object.keys(DEFAULT_EVALUATION_RUBRIC)];
+    //      columns.forEach(sortField => {
+    //        [true, false].forEach(sortAsc => {
+    //          const sort = this.table._makeSorter(sortField, sortAsc);
+    //          const sorted = this.table.questionStats.sort(sort);
+    //          success = success && verifySort(sortField, sortAsc, sorted);
+    //        });
+    //      });
+    //      await deleteCourse(courseData);
+    //      return success;
+    //    });
+  }
+
+  stateChange(e: Event) {
+    const state = e.detail.state.components[this.componentId];
+    state && (this.table = state.table);
+  }
 }
 
-async function saveCourse(course: Course): Course {
-  return null;
-}
+// Arbitraries
 
-function flatten(arr: any[]): any[] {
-  return arr ? Array.prototype.concat.apply([], arr) : arr;
-}
-
-function labelledIds(obj: any, T: string): {label: string, id: string}[] {
-  if (!obj || typeof obj !== 'object')
-    return [];
-  if (Array.isArray(obj))
-    return obj.map(el => labelledIds(el, T));
-  return flatten(
-    Object.keys(obj)
-    .filter(key => key != 'id')
-    .map(key => labelledIds(obj[key], TYPE_MAP[key]))
-  ).concat({type: T, id: obj.id});
-}
-
-function buildDeleteQuery(ids: {label: string, id: string}[]): string {
-  const params = ids.map((node, i) => `$id${node.type + i}: ID!`).join(', ');
-  return `
-    mutation del(${params}) {
-      ` + ids.map((node, i) => `
-        ${node.type + i}: delete${node.type} (id: $id${node.type + i}) { id }
-        `).join("\n") +
-    `}
-  `;
-}
-
-function buildDeleteVariables(ids: {label: string, id: string}[]): GQLVariables {
-  return ids.reduce((result, node, i) => {
-    return {...result, [node.label + i]: node.id}
-  });
-}
-
-async function deleteCourse(course: Course) {
-  const ids = labelledIds(course, 'Course');
-  const query = buildDeleteQuery(ids);
-  const variables = buildDeleteVariables(ids);
-  await GQLRequest(query, variables, JWT, handleError);
-}
-
+// Utils
 function setUpListener(table: PrendusCourseQuestionRatings): Promise {
   let _resolve, listener;
   const promise = new Promise((resolve, reject) => {
@@ -84,44 +121,7 @@ function setUpListener(table: PrendusCourseQuestionRatings): Promise {
   return promise;
 }
 
-// Arbitraries
-const categoryScoreArb = jsc.record({
-  category: jsc.elements(Object.keys(DEFAULT_EVALUATION_RUBRIC)),
-  score: jsc.nat(2)
-});
-const ratingArb = jsc.record({
-  raterId: jsc.constant(studentId),
-  scores: jsc.array(categoryScoreArb)
-});
-const disciplineArb = jsc.record({
-  title: jsc.asciinestring
-});
-const subjectArb = jsc.record({
-  title: jsc.asciinestring,
-  discipline: disciplineArb
-});
-const conceptArb = jsc.record({
-  title: jsc.asciinestring
-  subject: subjectArb
-});
-const questionArb = jsc.record({
-  text: jsc.asciinestring,
-  code: jsc.asciinestring,
-  concept: conceptArb,
-  ratings: jsc.array(ratingArb),
-  authorId: jsc.constant(studentId)
-});
-const assignmentArb = jsc.record({
-  title: jsc.asciinestring,
-  questions: jsc.array(questionArb),
-  authorId: jsc.constant(instructorId)
-});
-const courseArb = jsc.record({
-  title: jsc.asciinestring,
-  assignments: jsc.small(jsc.array(assignmentArb))
-});
-
-//Properties
+// Properties
 function verifyTable(table: PrendusCourseQuestionRatings, course: Course): boolean {
   return table.courseId === course.id
     && table.assignmentId === 'All'
@@ -157,101 +157,6 @@ function verifySort(sortField: number, sortAsc: number, sorted: QuestionRatingSt
       return result && prevStat >= nextStat;
     }
   }, true);
-}
-
-class PrendusCourseQuestionRatingsTest extends Polymer.Element {
-
-  static get is() { return 'prendus-course-question-ratings-test' }
-
-  constructor() {
-    super();
-    this.componentId = createUUID();
-    this.rootReducer = RootReducer;
-  }
-
-  connectedCallback() {
-    super.connectedCallback();
-    this.action = {
-      type: 'SET_COMPONENT_PROPERTY',
-      componentId: this.componentId,
-      key: 'table',
-      value: this.shadowRoot.querySelector('#table')
-    };
-  }
-
-  prepareTests(test) {
-
-    test('Set course id without residual state', [courseArb], async (course: Course) => {
-      const courseData = await saveCourse(course);
-      const table = new PrendusCourseQuestionRatings();
-      this.shadowRoot.appendChild(table);
-      const fulfilled = setUpListener(table);
-      table.courseId = courseData.id;
-      console.log('awaiting', time());
-      await fulfilled;
-      console.log('waited', time());
-      const success = verifyTable(table, courseData);
-      this.shadowRoot.removeChild(table);
-      await deleteCourse(courseData);
-      return success;
-    });
-
-    //    test('Set course id with residual state', [coursesArb], async (courses: Courses) => {
-    //      const coursesData = await saveCourses(courses);
-    //      console.log(coursesData);
-    //      let success = true;
-    //      coursesData.forEach(async (course) => {
-    //        const fulfilled = setUpListener(this.table);
-    //        this.table.courseId = courseId;
-    //        await fulfilled;
-    //        success = success && verifyTable(this.table, course);
-    //      });
-    //      await Promise.all(coursesData.map(deleteCourse));
-    //      return success;
-    //    });
-
-    test('Set assignment id and concept id', [courseArb], async (course: Course) => {
-      const courseData = await saveCourse(course);
-      const fulfilled = setUpListener(this.table);
-      this.table.courseId = courseData.id;
-      await fulfilled;
-      let success = true;
-      const conceptIds = ['All', ...this.table._concepts(courseData.assignments).map(concept => concept.id)];
-      const assignmentIds = ['All', ...courseData.assignments.map(assignment => assignment.id)];
-      assignmentIds.forEach(assignmentId => {
-        conceptIds.forEach(conceptId => {
-          const filter = this.table._makeFilter(assignmentId, conceptId);
-          const filtered = this.table.questionStats.filter(filter);
-          success = success && verifyFilter(assignmentId, conceptId, filtered);
-        });
-      });
-      await deleteCourse(courseData);
-      return success;
-    });
-
-    test('Sort columns', [courseArb], async (course: Course) => {
-      const courseData = await saveCourse(course);
-      const fulfilled = setUpListener(this.table);
-      this.table.courseId = courseData.id;
-      await fulfilled;
-      let success = true;
-      const columns = ['Overall', 'Student', ...Object.keys(DEFAULT_EVALUATION_RUBRIC)];
-      columns.forEach(sortField => {
-        [true, false].forEach(sortAsc => {
-          const sort = this.table._makeSorter(sortField, sortAsc);
-          const sorted = this.table.questionStats.sort(sort);
-          success = success && verifySort(sortField, sortAsc, sorted);
-        });
-      });
-      await deleteCourse(courseData);
-      return success;
-    });
-  }
-
-  stateChange(e: Event) {
-    const state = e.detail.state.components[this.componentId];
-    state && (this.table = state.table);
-  }
 }
 
 window.customElements.define(PrendusCourseQuestionRatingsTest.is, PrendusCourseQuestionRatingsTest)
---
+++ test/src/services/dataGen-service.ts
@@ -0,0 +1,127 @@
+import {GQLRequest} from '../../../../src/node_modules/prendus-shared/services/graphql-service';
+import {GQLVariables} from '../../../../src/typings/gql-variables';
+import {GQLArbitrary} from './services/arbitraries-service';
+
+const AUTH_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MDQxOTcxNzMsImNsaWVudElkIjoiY2o1bDg3cmQwMzVoaTAxMzQ0bzAwNW5maCIsInByb2plY3RJZCI6ImNqNW12aXNoaW5ucGYwMTM0OG04Z3p0YjAiLCJwZXJtYW5lbnRBdXRoVG9rZW5JZCI6ImNqNzBvNXJldTA0ZmEwMTk4a2ZlNnkwaXIifQ.LbuydRKQjQgbMiMggFU-wOr-IKSxzcO5ZA5mAZGEUjU';
+
+export async function authenticateTestUser(role: string): {id: string, token: string} {
+  const email = `test-${role}@test-prendus.com`;
+  const data = await GQLRequest(`mutation create($role: UserRole!, $email: String!) {
+    createUser(role: $role, authProvider: { email: { email: $email, password: "test" } }) {
+      id
+    }
+    signinUser(email: { email: $email, password: "test" }) {
+      token
+    }
+  }`, {role, email}, AUTH_TOKEN, handleError);
+  return {
+    id: data.createUser.id,
+    token: data.siginUser.token
+  }
+}
+
+export async function deleteTestUsers(...users: User[]) {
+  const params = users.map((user, i) => `$user${i}: ID!`).join(', ');
+  const query = `
+    del(${params}) {
+      ` + users.map((user, i) => `user${i}: deleteUser(id: $user${i}) { id }`) + `
+    }
+  `;
+  const variables = users.reduce((result, user, i) => {
+    return {
+      ...result,
+      [`user${i}`]: user.id
+    }
+  }, {});
+  await GQLRequest(query, variables, AUTH_TOKEN, handleError);
+}
+
+export async function saveArbitrary(arb: GQLArbitrary) {
+  const query = arbToCreateQuery(arb);
+  const variables = arbToVariables(arb);
+  const data = await GQLRequest(query, variables, AUTH_TOKEN, handleError);
+  return data[arb.type];
+}
+
+export async function deleteArbitrary(typedIds: object) {
+  const flattened = flattenDeleteVariables(typedIds);
+}
+
+function flattenDeleteVariables(typedIds: object): {type: string, id: string}[] {
+  const fn = (obj: any, T: string) => flatten(
+    Object.keys(obj)
+    .filter(k => k != 'id')
+    .map(k => {
+      return Array.isArray(obj[k])
+        ? obj[k].map(el => fn(el, k))
+        : fn(obj[k], k)
+    })
+  ).concat({type: T, id: obj.id});
+
+}
+
+function arbToCreateQuery(arb: GQLArbitrary): string {
+  const params = Object.keys(arb)
+    .filter(k => k != 'type')
+    .map(k => `$${k}:` + arbParamType(arb.type, k, arb[k]))
+    .join(', ');
+  return `
+    mutation saveArb(${params}) {
+      ${arb.type}: create${arb.type} (
+        ` + Object.keys(arb).filter(k => k != 'type').map(k => `${k}: $${k}`).join("\n") + `
+      ) {
+        ${collectArbIds(arb)}
+      }
+    }
+  `;
+}
+
+//TODO: need a good way to handle enums
+function arbParamType(containerType: string, fieldName: string, field: any): string {
+  if (Array.isArray(field))
+    return `[${containerType + fieldName + field[0].type}!]`
+  if (field && typeof field === 'object')
+    return `${containerType + fieldName + field.type}!`;
+  if (typeof field === 'string')
+    return 'String!';
+  return 'Int!';
+}
+
+function collectArbIds(arb: GQLArbitrary): string {
+  return `
+    id
+  ` + Object.keys(arb)
+      .filter(k => k != 'type')
+      .reduce((result, k) => {
+        if (['string', 'number'].includes(typeof arb[k]))
+          return result;
+        if (Array.isArray(arb[k]))
+          return result + `
+            ${arb[k][0].type}: ${k} { ${collectArbIds(arb[k][0])}
+          }`;
+        return result + `
+          ${arb[k].type}: ${k} { ${collectArbIds(arb[k])}
+        }`;
+      }, '');
+}
+
+function arbToVariables(arb: GQLArbitrary): GQLVariables {
+  const vars = {...arb};
+  if (vars.type) delete vars.type;
+  Object.keys(arb).filter(k => k != 'type').forEach(k => {
+    if (Array.isArray(arb[k]))
+      vars[k] = arb[k].map(arbToVariables);
+    else if (typeof arb[k] === 'object')
+      vars[k] = arbToVariables(arb[k]);
+  });
+  return vars;
+}
+
+function handleError(err: any) {
+  console.error(err);
+}
+
+function flatten(arr: any[]): any[] {
+  return arr ? Array.prototype.concat.apply([], arr) : arr;
+}
+
