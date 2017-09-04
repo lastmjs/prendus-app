0abcc9e564d96fcba8b35625b360acfbdd71977d
Benjamin Ogles <benjaminogles@gmail.com>
2017-09-04 08:52:17 -0600

    Fix analytics service

--- src/services/analytics-service.ts
+++ src/services/analytics-service.ts
@@ -1,35 +1,23 @@
-import {GQLMutate} from '../services/graphql-service';
+import {GQLRequest} from '../services/graphql-service';
 
-export async function sendStatement(userId: string, contextId: string, contextType: string, verb: string, object: string): Promise<void> {
-  const createStatement = await GQLMutate(`
-    mutation{
+export async function sendStatement(userId: string, contextId: string, contextType: string, verb: string, obj: string): Promise<void> {
+  const createStatement = await GQLRequest(`
+    mutation analytics($contextId: String!, $contextType: ContextType!, $verb: String!, $obj: String!, $userId: ID!) {
       createPrendusAnalytics(
-        contextId: "${contextId}"
-        contextType: ${contextType}
-        verb: "${verb}"
-        object: "${object}"
+        contextId: $contextId
+        contextType: $contextType
+        verb: $verb
+        object: $obj
+        userId: $userId
       ){
         id
       }
     }
-    `, this.userToken, (error: any) => {
-        console.log(error);
-  });
-  const data = await GQLMutate(`
-    mutation {
-      addToUserOnPrendusAnalytics(
-        userUserId: "${userId}"
-        userAnalyticsPrendusAnalyticsId: "${createStatement.createPrendusAnalytics.id}"
-      ){
-        userUser{
-          id
-        }
-        userAnalyticsPrendusAnalytics {
-          id
-        }
-      }
+    `,
+    {userId, contextId, contextType, verb, obj},
+    '', //TODO: pass in user token or retrieve it from redux
+    (error: any) => {
+      console.log(error);
     }
-    `, this.userToken, (error: any) => {
-        console.log(error);
   });
 }
