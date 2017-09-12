1f2f76e9db86446d0fa89f9afb16662440103cc8
Jordan Last <jordan.michael.last@gmail.com>
2017-09-12 14:44:21 -0600

    get rid of the old demo and start the new demo

---
+++ src/components/prendus-assignment/prendus-create-assignment-demo.ts
@@ -0,0 +1,5 @@
+class PrendusCreateAssignmentDemo extends Polymer.Element {
+    static get is() { return 'prendus-create-assignment-demo'; }
+}
+
+window.customElements.define(PrendusCreateAssignmentDemo.is, PrendusCreateAssignmentDemo);
