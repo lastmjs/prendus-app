4b83686b1a114183e44791e4ba49179419801437
Jordan Last <jordan.michael.last@gmail.com>
2017-09-12 16:10:43 -0600

    steps 0 and 1 basic structure complete

--- src/components/prendus-assignment/prendus-create-assignment-demo.ts
+++ src/components/prendus-assignment/prendus-create-assignment-demo.ts
@@ -1,5 +1,44 @@
+import {createUUID} from '../../services/utilities-service';
+import {SetComponentPropertyAction} from '../../typings/actions';
+import {State} from '../../typings/state';
+
 class PrendusCreateAssignmentDemo extends Polymer.Element {
+    componentId: string;
+    selected: SetComponentPropertyAction;
+    action: SetComponentPropertyAction;
+
     static get is() { return 'prendus-create-assignment-demo'; }
+
+    constructor() {
+      super();
+      this.componentId = 'asdf9s8d9g8s9dg';
+    //   this.componentId = createUUID(); //TODO this was stopping the component from loading for some reason
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+
+        this.action = fireLocalAction(this.componentId, 'selected', 0);
+    }
+
+    selectedChanged(e: CustomEvent) {
+        this.action = fireLocalAction(this.componentId, 'selected', e.detail.value);
+    }
+
+    stateChange(e: CustomEvent) {
+        const state: State = e.detail.state;
+
+        if (state.components[this.componentId]) this.selected = state.components[this.componentId].selected;
+    }
 }
 
 window.customElements.define(PrendusCreateAssignmentDemo.is, PrendusCreateAssignmentDemo);
+
+function fireLocalAction(componentId: string, key: string, value: any): SetComponentPropertyAction {
+    return {
+        type: 'SET_COMPONENT_PROPERTY',
+        componentId,
+        key,
+        value
+  };
+}
