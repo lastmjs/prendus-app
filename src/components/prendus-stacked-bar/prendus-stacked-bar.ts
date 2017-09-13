40e41d0a3607b45118c4b529267e0cea103cc88b
Benjamin Ogles <benjaminogles@gmail.com>
2017-09-13 17:13:53 -0600

    Refactored question rating table

--- src/components/prendus-stacked-bar/prendus-stacked-bar.ts
+++ src/components/prendus-stacked-bar/prendus-stacked-bar.ts
@@ -1,11 +1,41 @@
+import {createUUID} from '../../node_modules/prendus_shared/services/utilities-service';
+
 class PrendusStackedBar extends Polymer.Element {
+  data: number[];
   static get is() { return 'prendus-stacked-bar' }
 
   static get properties() {
     return {
-      data: Array,
+      scores: {
+        type: Array,
+        observer: '_scoresChanged'
+      },
       label: String
     }
+  }
+
+  constructor() {
+    super();
+    this.componentId = createUUID();
+  }
+
+  _fireLocalAction(key: string, value: any) {
+    this.action = {
+      type: 'SET_COMPONENT_PROPERTY',
+      componentId: this.componentId,
+      key,
+      value
+    }
+  }
+
+  _scoresChanged(scores: CategoryScore[]) {
+    const data = scores.map(obj => obj.score).reduce((result, score) => {
+      const tailLen = score - result.length + 1;
+      const tail = tailLen > -1 ? Array(tailLen).fill(0) : [];
+      const updated = [...result, ...tail];
+      return [...updated.slice(0, score), updated[score]+1, ...updated.slice(score+1)];
+    }, []);
+    this._fireLocalAction('data', data);
   }
 
   _computeWidth(data: number[], num: number) {
@@ -14,6 +44,12 @@ class PrendusStackedBar extends Polymer.Element {
     return 'width: ' + (num / total * 100) + '%;';
   }
 
+  stateChange(e: CustomEvent) {
+    const state = e.detail.state;
+    const componentState = state.components[this.componentId] || {};
+    const keys = Object.keys(componentState);
+    if (keys.includes('data')) this.data = componentState.data;
+  }
 }
 
 window.customElements.define(PrendusStackedBar.is, PrendusStackedBar)
