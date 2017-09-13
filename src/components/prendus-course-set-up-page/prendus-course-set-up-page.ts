85c6c11ebf792bec2ed4dec50a340b0caac24f96
trevorhere <trevoranthonylane@gmail.com>
2017-09-13 11:56:56 -0600

    creation of course set up instructions page

---
+++ src/components/prendus-course-set-up-page/prendus-course-set-up-page.ts
@@ -0,0 +1,6 @@
+
+class PrendusCourseSetUpPage extends Polymer.Element {
+    static get is() { return 'prendus-course-set-up-page'; }
+}
+
+window.customElements.define(PrendusCourseSetUpPage.is, PrendusCourseSetUpPage);
