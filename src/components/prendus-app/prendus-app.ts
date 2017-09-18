72d9aa55c5051ed0808a78a6ecdfa179ad302356
Ben Mackley <benjaminmackley@gmail.com>
2017-09-18 14:06:14 -0600

    added a few pages

--- src/components/prendus-app/prendus-app.ts
+++ src/components/prendus-app/prendus-app.ts
@@ -27,7 +27,7 @@ class PrendusApp extends Polymer.Element {
         this.action = await getAndSetUser();
     }
 
-    getSelectedView(rootRouteActive: any, whatIsPrendusRouteActive: any, courseSetupRouteActive: any, howItWorksRouteActive: any, coursesRouteActive: any, createCourseRouteActive: any, viewCourseRouteActive: any, editCourseRouteActive: any, paymentCourseRouteActive: any, courseQuestionRatingsRouteActive: any, createAssignmentRouteActive: any, assignmentCreateRouteActive: any, assignmentReviewRouteActive: any, assignmentGradeRouteActive: any, assignmentQuizRouteActive: any, editAssignmentRouteActive: any, createDisciplineRouteActive: any, viewDisciplineRouteActive: any, editDisciplineRouteActive: any, createSubjectRouteActive: any, viewSubjectRouteActive: any, editSubjectRouteActive: any, createConceptRouteActive: any, viewConceptRouteActive: any, editConceptRouteActive: any, teacherApprovalRouteActive: any, learningStructureRouteActive: any, signupRouteActive: any, loginRouteActive: any, authenticateRouteActive: any, viewQuestionRouteActive: any, createQuestionRouteActive: any, editQuestionRouteActive: any, editDemoQuestionRouteActive: any, examplesQuestionRouteActive: any, openSourceRouteActive: any, scapholdDemoRouteActive: any) {
+    getSelectedView(rootRouteActive: any, whatIsPrendusRouteActive: any, courseSetupRouteActive: any, howItWorksRouteActive: any, researchBackgroundRouteActive: any, coursesRouteActive: any, createCourseRouteActive: any, viewCourseRouteActive: any, editCourseRouteActive: any, paymentCourseRouteActive: any, courseQuestionRatingsRouteActive: any, createAssignmentRouteActive: any, assignmentCreateRouteActive: any, assignmentReviewRouteActive: any, assignmentGradeRouteActive: any, assignmentQuizRouteActive: any, editAssignmentRouteActive: any, createDisciplineRouteActive: any, viewDisciplineRouteActive: any, editDisciplineRouteActive: any, createSubjectRouteActive: any, viewSubjectRouteActive: any, editSubjectRouteActive: any, createConceptRouteActive: any, viewConceptRouteActive: any, editConceptRouteActive: any, teacherApprovalRouteActive: any, learningStructureRouteActive: any, signupRouteActive: any, loginRouteActive: any, authenticateRouteActive: any, viewQuestionRouteActive: any, createQuestionRouteActive: any, editQuestionRouteActive: any, editDemoQuestionRouteActive: any, examplesQuestionRouteActive: any, openSourceRouteActive: any, scapholdDemoRouteActive: any) {
         this.action = checkForUserToken();
         if (rootRouteActive){
           if(this.userToken){
@@ -56,6 +56,11 @@ class PrendusApp extends Polymer.Element {
         window.ga('set','page', this.route.path);
         window.ga('send','pageview');
         return 'howItWorksView';
+        }
+        if(researchBackgroundRouteActive){
+          window.ga('set','page', this.route.path);
+          window.ga('send','pageview');
+          return 'researchBackgroundView';
         }
         if (signupRouteActive){
           window.ga('set', 'page', this.route.path);
---
+++ src/components/prendus-marketing/prendus-research-background.ts
@@ -0,0 +1,5 @@
+class PrendusResearchBackGround extends Polymer.Element {
+     static get is() { return 'prendus-research-background'; }
+ }
+
+ +window.customElements.define(PrendusResearchBackGround.is, PrendusResearchBackGround);
