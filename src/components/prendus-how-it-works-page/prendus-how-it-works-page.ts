5f19dfb7533c74acf53608e85449192aacc12911
Ben Mackley <benjaminmackley@gmail.com>
2017-09-28 10:08:24 -0600

    minor changes

--- src/components/prendus-how-it-works-page/prendus-how-it-works-page.ts
+++
@@ -1,6 +0,0 @@
-
-class PrendusHowItWorksPage extends Polymer.Element {
-    static get is() { return 'prendus-how-it-works-page'; }
-}
-
-window.customElements.define(PrendusHowItWorksPage.is, PrendusHowItWorksPage);
