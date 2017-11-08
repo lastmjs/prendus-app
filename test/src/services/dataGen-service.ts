14ff1d0d2ad392af539ca3ba93aee9f87ebf8bf8
Benjamin Ogles <benjaminogles@gmail.com>
2017-11-08 16:21:45 -0700

    Clean up code for dataGen service

--- test/src/services/dataGen-service.ts
+++ test/src/services/dataGen-service.ts
@@ -2,7 +2,13 @@ import {
   User,
   Course
 } from '../../../../src/typings/index.d';
+import {schema} from '../graphcool/testSchema';
 import {GQLRequest} from '../../../../src/node_modules/prendus-shared/services/graphql-service';
+
+const {
+  getNamedType,
+  GraphQLObjectType,
+} = require('graphql');
 
 const AUTH_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MDQxOTcxNzMsImNsaWVudElkIjoiY2o1bDg3cmQwMzVoaTAxMzQ0bzAwNW5maCIsInByb2plY3RJZCI6ImNqNW12aXNoaW5ucGYwMTM0OG04Z3p0YjAiLCJwZXJtYW5lbnRBdXRoVG9rZW5JZCI6ImNqNzBvNXJldTA0ZmEwMTk4a2ZlNnkwaXIifQ.LbuydRKQjQgbMiMggFU-wOr-IKSxzcO5ZA5mAZGEUjU';
 
@@ -19,6 +25,116 @@ const dependencyTree = [
 
 function dependencySort(typeA, typeB): number {
   return dependencyTree.indexOf(typeA) < dependencyTree.indexOf(typeB) ? 1 : -1;
+}
+
+export async function saveArbitrary(arb, name: string): Promise<object> {
+  const mut = schema.getMutationType().getFields()[name];
+  const gql = mutation(arb, mut, name);
+  const data = await GQLRequest(gql, arb, AUTH_TOKEN, handleError);
+  return data[name];
+}
+
+export async function deleteArbitrary(data, name: string) {
+  const mut = schema.getMutationType().getFields()[name];
+  const typedIds = flattenTypedIds(data, mut.type);
+  const variables = deleteVariables(typedIds);
+  const gql = deleteGql(typedIds);
+  return GQLRequest(gql, variables, AUTH_TOKEN, handleError);
+}
+
+function operationParameters(arb, mut) {
+  return Object.keys(arb)
+    .map(field => {
+      const i = mut.args.findIndex(arg => arg.name === field);
+      if (i === -1)
+        throw new Error(`Field ${field} is not in type ${mut.type.toString()}`);
+      return `$${field}: ${mut.args[i].type.toString()}`;
+    })
+    .join('\n');
+}
+
+function mutationParameters(arb, mut) {
+  return Object.keys(arb)
+    .map(field => {
+      if (!mut.args.some(arg => arg.name === field))
+        throw new Error(`Field ${field} is not in type ${mut.type.toString()}`);
+      return `${field}: $${field}`;
+    })
+    .join('\n');
+}
+
+function mutationSelections(arb, type, name) {
+  if (Array.isArray(arb) && arb.every(el => typeof el === 'object'))
+    return mutationSelections(arb[0], type, name);
+  else if (Array.isArray(arb) || typeof arb !== 'object')
+    return '';
+  const gqlFields = getNamedType(type).getFields();
+  const fields = Object.keys(arb).filter(field => Boolean(gqlFields[field]));
+  const selections = fields
+    .map(
+      field => mutationSelections(arb[field], gqlFields[field].type, field)
+    )
+    .join('\n');
+  return `
+    ${name} {
+      id
+      ${selections}
+    }
+  `;
+}
+
+function mutation(arb, mut, name) {
+  return `mutation saveArbitrary(
+    ${operationParameters(arb, mut)}
+  ) {
+    ${name} (
+      ${mutationParameters(arb, mut)}
+    ) ${mutationSelections(arb, mut.type, '')}
+  }
+  `;
+}
+
+function deleteVariables(typedIds) {
+  return typedIds.reduce(
+    (result, { type, id }) => ({
+      ...result,
+      [id]: id
+    }),
+    {}
+  );
+}
+
+function deleteGql(typedIds) {
+  const params = typedIds.map(({ id }) => `$${id}: ID!`).join(', ');
+  return `
+    mutation del(${params}) {
+      ` + typedIds
+    .map(
+      ({ type, id }, i) => `
+        ${type}${i}: delete${type}(id: $${id}) {
+          id
+        }
+      `
+    )
+    .join("\n")
+      + `
+    }
+  `;
+}
+
+function flattenTypedIds(data, type) {
+  if (Array.isArray(data))
+    return data
+      .map(datum => flattenTypedIds(datum, type))
+      .reduce(flatten, []);
+  const rawType = getNamedType(type);
+  const fields = rawType.getFields();
+  return Object.keys(data).reduce(
+    (result, k) => k === 'id'
+      ? [ ...result, { type: rawType.name, id: data.id } ]
+      : [ ...result, ...flattenTypedIds(data[k], fields[k].type) ],
+    []
+  ).sort((a, b) => dependencySort(a.type, b.type));
 }
 
 export async function createTestUser(role: string): Promise<User> {
