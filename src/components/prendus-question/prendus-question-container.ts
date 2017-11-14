5230f0a003dcfb9fcd22894b836ceb1402b47d7b
Ben Mackley <benjaminmackley@gmail.com>
2017-11-14 10:40:19 -0700

    at a somewhat stable place

--- src/components/prendus-question/prendus-question-container.ts
+++ src/components/prendus-question/prendus-question-container.ts
@@ -9,21 +9,26 @@ import {setNotification, getAndSetUser, checkForUserToken} from '../../redux/act
 import {LTIPassback} from '../../services/lti-service';
 import {sendStatement} from '../../services/analytics-service';
 import {GQLRequest} from '../../node_modules/prendus-shared/services/graphql-service';
-import {Assignment} from '../../typings/assignment'
 
-class PrendusQuestionsView extends Polymer.Element {
+class PrendusQuestionContainer extends Polymer.Element {
   loaded: boolean;
   action: SetPropertyAction | SetComponentPropertyAction | DefaultAction;
   componentId: string;
   userToken: string;
   user: User;
+  assignment: Assignment;
+  flagQuestionModalOpened: boolean;
   noUserQuestions: boolean;
   questionIds: string[];
   questions:  Question[];
-  static get is() { return 'prendus-questions-view' }
+  static get is() { return 'prendus-question-container' }
 
   static get properties() {
-
+    return {
+        questionId: {
+            type: String,
+        },
+    };
   }
 
   constructor() {
@@ -32,60 +37,26 @@ class PrendusQuestionsView extends Polymer.Element {
   }
 
   async connectedCallback() {
-    this.action = fireLocalAction(this.componentId, 'loaded', false)
-    await this.loadQuizzes();
     super.connectedCallback();
+    this.action = fireLocalAction(this.componentId, 'loaded', false)
+    this.action = fireLocalAction(this.componentId, 'loaded', true)
   }
-
-  async loadQuizzes(){
-    try{
-      setTimeout(async () => {
-        this.action = checkForUserToken();
-        this.action = await getAndSetUser();
-        const userQuestionIds = await getUserQuestions(this.user.id, this.userToken);
-        const userQuestionIdsArray = (userQuestionIds) ? userQuestionIds.map(question => question.id) : false;
-        this.action = fireLocalAction(this.componentId, 'questionIds', userQuestionIdsArray)
-        this.action = fireLocalAction(this.componentId, 'loaded', true)
-      }, 0);
-    }catch(error){
-      this.action = setNotification(error.message, NotificationType.ERROR);
-      this.action = fireLocalAction(this.componentId, 'loaded', true)
-    }
-  }
-
 
   stateChange(e: CustomEvent) {
     const state = e.detail.state;
     const componentState = state.components[this.componentId] || {};
     const keys = Object.keys(componentState);
     if (keys.includes('loaded')) this.loaded = componentState.loaded;
-    if (keys.includes('questions')) this.quizzes = componentState.quizzes;
+    if (keys.includes('assignment')) this.assignment = componentState.assignment;
+    if (keys.includes('questions')) this.questions = componentState.questions;
     if (keys.includes('questionIds')) this.questionIds = componentState.questionIds;
     if (keys.includes('noUserQuestions')) this.noUserQuestions = componentState.noUserQuestions;
     if (keys.includes('error')) this.error = componentState.error;
+    if (keys.includes('flagQuestionModalOpened')) this.flagQuestionModalOpened = componentState.flagQuestionModalOpened;
     this.userToken = state.userToken;
     this.user = state.user;
   }
 
 }
 
-window.customElements.define(PrendusQuestionsView.is, PrendusQuestionsView)
-
-async function getUserQuestions(userId: String, userToken: String) {
-    const data = await GQLRequest(`
-      query getQuestions($userId: ID!) {
-        allQuestions(
-          first: 10
-          filter: {
-            author: {
-              id: $userId
-            }
-        }) {
-          id
-        }
-      }
-    `, {userId}, userToken, (error: any) => {
-      throw error;
-    });
-    return data.allQuestions;
-}
+window.customElements.define(PrendusQuestionContainer.is, PrendusQuestionContainer)
