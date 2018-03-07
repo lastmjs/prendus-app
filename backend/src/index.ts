41cabd65039d056c923dd9e6c5bc48d5af75ba30
Jordan Last <jordan.michael.last@gmail.com>
2018-03-07 12:51:51 -0700

    automatically adding all of the generated queries to the application query and mutation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,7 @@
 import { GraphQLServer } from 'graphql-yoga';
 import { Prisma } from './generated/prisma';
+import {readFileSync} from 'fs';
+import {parse, print} from 'graphql';
 
 //TODO prepare an automatic mapping of resolvers to the generated resolvers. I don't want to have to manually add each resolver. Do like we did in GraphSM
 //TODO we can object spread the prepared resolvers into the Mutation and Query properties here, allowing us to add custom resolvers if we like
@@ -8,22 +10,65 @@ import { Prisma } from './generated/prisma';
 
 const PrismaDBConnection = new Prisma({
     endpoint: 'http://127.0.0.1:4466/backend/dev', // the endpoint of the Prisma DB service
-    secret: 'mysecret123', // specified in database/prisma.yml
+    secret: 'mysecret123', // specified in database/prisma.yml //TODO obviously this should be controlled with environment variables
     debug: true, // log all GraphQL queries & mutations
 });
 const preparedQuery = prepareResolvers(PrismaDBConnection.query);
 const preparedMutation = prepareResolvers(PrismaDBConnection.mutation);
 const resolvers = {
     Query: {
-        ...preparedQuery
+        ...preparedQuery,
+        monkey: () => {
+            return 1;
+        }
     },
     Mutation: {
-        ...preparedMutation
+        ...preparedMutation,
+        createMonkey: () => {
+            return 5;
+        }
     }
 };
 
+const generatedSchemaAST = parse(readFileSync('./src/generated/prisma.graphql').toString());
+const augmentedQueryAndMutationSchemaAST = parse(`
+    type Query {
+        monkey: Int!
+    }
+
+    type Mutation {
+        createMonkey: Int!
+    }
+`);
+const augmentedSchemaAST = {
+    ...generatedSchemaAST,
+    definitions: generatedSchemaAST.definitions.map((generatedSchemaDefinition) => {
+        const matchingDefinitionInAugmentedQuerySchema = augmentedQueryAndMutationSchemaAST.definitions.filter((augmentedQueryAndMutationSchemaDefinition) => {
+            return (
+                generatedSchemaDefinition.kind === 'ObjectTypeDefinition' &&
+                augmentedQueryAndMutationSchemaDefinition.kind === 'ObjectTypeDefinition' &&
+                generatedSchemaDefinition.name.kind === 'Name' &&
+                augmentedQueryAndMutationSchemaDefinition.name.kind === 'Name' &&
+                generatedSchemaDefinition.name.value === augmentedQueryAndMutationSchemaDefinition.name.value
+            );
+        })[0];
+
+        if (matchingDefinitionInAugmentedQuerySchema) {
+            const augmentedSchemaDefinition = {
+                ...generatedSchemaDefinition,
+                fields: [...generatedSchemaDefinition.fields, ...matchingDefinitionInAugmentedQuerySchema.fields]
+            };
+
+            return augmentedSchemaDefinition;
+        }
+        else {
+            return generatedSchemaDefinition;
+        }
+    })
+};
+
 const server = new GraphQLServer({
-    typeDefs: './generated/prisma.graphql',
+    typeDefs: print(augmentedSchemaAST),
     resolvers
 });
 
@@ -33,11 +78,14 @@ function prepareResolvers(resolverObject) {
     return Object.entries(resolverObject).reduce((result, entry) => {
             const resolverName = entry[0];
             const resolverFunction = entry[1];
+
+            console.log();
+
             return {
                 ...result,
                 [resolverName]: async (parent, args, context, info) => {
                     return await resolverFunction(args, info);
                 }
             };
-    });
+    }, {});
 }
