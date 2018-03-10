b20490ca3b07e5581c75458c22edae2d418aecdb
Jordan Last <jordan.michael.last@gmail.com>
2018-03-09 17:47:04 -0700

    about to fix zwitterion to allow bare imports

--- test/backend/directive-resolvers/authenticated-directive-resolver-test.ts
+++ test/backend/directive-resolvers/authenticated-directive-resolver-test.ts
@@ -1,18 +1,19 @@
 // import {authenticatedDirectiveResolver} from '../../../backend/directive-resolvers';
 import {GraphQLClient} from '../../../node_modules/graphql-request/dist/src/index';
 
-import * as jsverify from 'jsverify';
+// import * as jsverify from 'jsverify';
 // import * as jsverify from '../../../node_modules/jsverify/lib/jsverify';
-// const client = new GraphQLClient(`http://localhost:4000`);
+const client = new GraphQLClient(`http://localhost:4000`);
 // const jsverify = require('../../../node_modules/jsverify/lib/jsverify');
 // const jsverify = require('../../../node_modules/jsverify/lib/jsverify');
 
-// const jsverify = require('jsverify');
+const jsverify = require('jsverify');
 
 class AuthenticatedDirectiveResolverTest extends HTMLElement {
     prepareTests(test: any) {
         test('Try it out', [jsverify.number], (arbNumber: number) => {
             console.log(arbNumber);
+            return true;
         });
     }
 }
